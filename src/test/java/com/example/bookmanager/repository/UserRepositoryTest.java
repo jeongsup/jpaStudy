5e8b767e6dd6e348cbb8d71f927d42f761de7395
ggobugi <ninjaggobugi@naver.com>
2022-02-06 22:21:50 +0900

    Refactor 불필요한 import 정리

--- src/test/java/com/example/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/example/bookmanager/repository/UserRepositoryTest.java
@@ -2,13 +2,13 @@ package com.example.bookmanager.repository;
 
 import com.example.bookmanager.domain.User;
 import jdk.nashorn.internal.ir.annotations.Ignore;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.*;
-
-import java.util.List;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
